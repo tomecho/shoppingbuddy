5fbf655d342b3e8c853305cdebc5ef6fafc6399e
Tom Peck <tapeck@umass.edu>
2017-02-03 02:13:58 -0500

    added sql query but im running into a problem

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -52,25 +52,28 @@ myApp.onPageInit('addItem', function (page) {
 });
 
 myApp.onPageInit('shoppingList', function (page) {
-  myApp.virtualList(".virtual-list-items", {
-    items: [
-      {
-        name: "alpaca",
-        price: "$4.00",
-        description: "A sturdy beast of burdeon!"
-      }
-    ],
-    template: '<li class="item-content">' +
-                //'<div class="item-media"><img src="{{picture}}"></div>' +
-                '<div class="item-inner">' +
-                    '<div class="item-title-row">' +
-                      '<div class="item-title">{{name}}</div>' +
-                      '<div class="item-after">{{price}}</div>' +
-                    '</div>' +
-                    '<div class="item-text">{{description}}</div>' +
-                '</div>' +
-             '</li>',
-    height: 80
-  });
+  $scope.db.transaction(function (tx) {
+    tx.executeSql("SELECT * from shoppingbuddy", function (tx, results) {
+      // for some reason need to convert this
+      var shoppingList = [];
+      //for(var i=0; i<results.rows.length; i++) {
+        //items.push(results.rows.item(i));
+      //}
 
+      myApp.virtualList(".virtual-list-items", {
+        items: shoppingList,
+        template: '<li class="item-content">' +
+                    //'<div class="item-media"><img src="{{picture}}"></div>' +
+                    '<div class="item-inner">' +
+                        '<div class="item-title-row">' +
+                          '<div class="item-title">{{name}}</div>' +
+                          '<div class="item-after">{{price}}</div>' +
+                        '</div>' +
+                        '<div class="item-text">{{description}}</div>' +
+                    '</div>' +
+                 '</li>',
+        height: 65
+      });
+    });
+  }, function (err) { alert(JSON.stringify(err)) });
 });
